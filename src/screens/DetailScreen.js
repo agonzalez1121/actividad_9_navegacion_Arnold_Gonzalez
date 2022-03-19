1ba6d81abce68df7ce89c76fa1edb498008db5e9
Arnold Gonzalez V <a1121250001@utch.edu.mx>
2022-03-18 19:31:58 -0700

    Actividad de Navegacion

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { View, Text, Button, SafeAreaView } from "react-native";
 
 export default function SettingsScreen({ route, navigation }) {
    const { itemId } = route.params;
+   const[post, setPost] =useState([]);
+
+   const getPost = async() => {
+    try{
+      const url = `https://jsonplaceholder.typicode.com/posts/${itemId}`;
+      const response = await fetch(url); //consumoir los datos
+      const json = await response.json(); //convertir a json
+      setPost(json);
+    } catch (error) {
+      console.error(error);
+    } 
+  }
+
+  useEffect(()=>{
+    getPost();
+  },[post])
+
   return (
     <SafeAreaView>
-      <Text>Detalle {JSON.stringify(itemId)}</Text>
+      <Text>Id:{JSON.stringify(itemId)}</Text>
+      <Text>Titulo:{post.title}</Text>
+      <Text>Body:{post.body}</Text>
+
+
       <Button onPress={() =>{
         navigation.navigate('Home');
       }} title="REGRESAR" />
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { View, Text, Button, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, SafeAreaView,FlatList } from "react-native";
 
 export default function HomeScreen({navigation }) {
+const [datos, setDatos] = useState([]);
+
+const getPosts = async() => {
+  try{
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url); //consumoir los datos
+    const json = await response.json(); //convertir a json
+    setDatos(json);
+  } catch (error) {
+    console.error(error);
+  } 
+}
+
+useEffect(()=>{
+  getPosts();
+}, [])
   return (
     <SafeAreaView>
-      <Text>Estamos en HomeScreen</Text>
+      <FlatList 
+              data={datos}
+              keyExtractor = { ({ id }, index) => id }
+              renderItem = {
+                ({item}) => (
+                  <Text onPress={
+                    ()=>{
+                      navigation.navigate('Detail',{
+                        itemId:item.id
+                      })
+                    }
+                  }>{item.title}</Text>
+                )
+              }
+      />
       <Button onPress={()=>{
 
          navigation.navigate('Detail', {
